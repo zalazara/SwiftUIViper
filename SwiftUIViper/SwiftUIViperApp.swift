3df794bd69e10408671f85dd22b7cb08eae624f5
Alejandro Zalazar <azalazar@outlook.com.ar>
2022-09-15 19:37:08 -0300

    Initial Commit

---
+++ SwiftUIViper/SwiftUIViperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViperApp.swift
+//  SwiftUIViper
+//
+//  Created by Alejandro Zalazar on 15/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
